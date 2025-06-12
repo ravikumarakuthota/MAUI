922549706608193b706e18687189e12a354f5c25
ravikumarakuthota <94777017+ravikumarakuthota@users.noreply.github.com>
2025-06-12 22:34:24 +0530

    formatted docs

--- MAUITasker/MVVM/Models/Category.cs
+++ MAUITasker/MVVM/Models/Category.cs
@@ -10,6 +10,6 @@ public class Category : ContentPage
 	public bool IsSelected { get; set; }
 	public Category()
 	{
-		
+
 	}
 }
--- MAUITasker/MVVM/Models/MyTask.cs
+++ MAUITasker/MVVM/Models/MyTask.cs
@@ -9,6 +9,6 @@ public class MyTask : ContentPage
 
 	public MyTask()
 	{
-		
+
 	}
 }
